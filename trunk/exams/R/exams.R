fffccbcbf73816ad93bd311d809aed1d024ee093
gruen (Bettina Gruen) <Bettina.Gruen@wu.ac.at>
2008-10-27 08:17:50 +0000

    update exams.Rnw, add mchoice.control
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/exams@100 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- trunk/exams/R/exams.R
+++ trunk/exams/R/exams.R
@@ -1,7 +1,7 @@
 ## workhorse function for compiling (collections of) exercises
 exams <- function(file, n = 1, dir = NULL, template = "plain",
   inputs = NULL, header = list(Date = Sys.Date()), name = NULL,
-  quiet = TRUE, edir = NULL, tdir = NULL)
+  quiet = TRUE, edir = NULL, tdir = NULL, mchoice.control = NULL)
 {
   ## convenience function
   strip_path <- function(file)
@@ -86,7 +86,7 @@ exams <- function(file, n = 1, dir = NULL, template = "plain",
     tol <- if(is.null(tol)) 0 else as.numeric(tol)
     sol <- if(mchoice) string2mchoice(sol) else as.numeric(sol)  
     slength <- length(sol)
-    string <- if(mchoice) paste(nam, ": ", paste(letters[which(sol)], collapse = ""), sep = "") else {
+    string <- if(mchoice) paste(nam, ": ", mchoice2print(sol), sep = "") else {
       if(tol <= 0) paste(nam, ": ", sol, sep = "") else {
         if(slength == 1) paste(nam, ": ", sol, " (", sol-tol, "--", sol+tol, ")", sep = "") else {
 	  paste(nam, ": [", sol[1], ", ", sol[2], "] ([", sol[1]-tol, "--", sol[1]+tol, ", ",
@@ -101,9 +101,19 @@ exams <- function(file, n = 1, dir = NULL, template = "plain",
          string = string)
   }
 
+  mchoice.default <- list(print = list(True = letters[1:5], False = rep("", 5)),
+                          symbol = c(True = "X", False = " "))
+  if (is.null(mchoice.control)) mchoice.control <- mchoice.default
+  else if (is(mchoice.control, "list")) {
+    mchoice.control <- c(mchoice.control, mchoice.default[!c("print", "symbol") %in% names(mchoice.control)])
+    if (!all(sapply(mchoice.control, function(x) identical(c("False", "True"), sort(names(x))))))
+      stop("'mchoice.control' not correctly specified")
+    mchoice.control$print <- lapply(mchoice.control$print, function(x) rep(x, 5))
+  } else stop("'mchoice.control' must be NULL or a list")
+  
   ## convenience functions for writing LaTeX  
   mchoice2quest <- function(x) paste("  \\item \\exmchoice{",
-    paste(ifelse(x, "X", " "), collapse = "}{"), "}", sep = "")
+    paste(ifelse(x, mchoice.control$symbol[["True"]], mchoice.control$symbol[["False"]]), collapse = "}{"), "}", sep = "")
   num2quest <- function(x) {
     rval <-  paste("  \\item \\exnum{", 
       paste(strsplit(format(c(100000.000, x), nsmall = 3, scientific = FALSE)[-1], "")[[1]][-7],
@@ -111,9 +121,11 @@ exams <- function(file, n = 1, dir = NULL, template = "plain",
     if(length(x) > 1) rval <- paste(rval, " \\\\\n        \\exnum{",
       paste(strsplit(format(c(100000.000, x), nsmall = 3, scientific = FALSE)[-1], "")[[2]][-7],
       collapse = "}{"), "}", sep = "")
-    rval
+    rval 
   }
-  
+
+  mchoice2print <- function(x) paste(ifelse(x, mchoice.control$print[["True"]], mchoice.control$print[["False"]]), collapse = "")
+
   ## take everything to temp dir
   file.copy(file_path, dir_temp)
   ## including further inputs (if any)
