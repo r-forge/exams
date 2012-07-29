ae12c208247f540e4b569dea81e820a038c68fc6
azeileis (Achim Zeileis) <Achim.Zeileis@R-project.org>
2012-07-29 11:35:58 +0000

    add tth.control function
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/exams@217 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/htmltools/R/tex2html.R
+++ pkg/htmltools/R/tex2html.R
@@ -28,22 +28,73 @@ TTH <- TTM <- ""
     }
 }
 
-tth <- function(x, delblanks = TRUE)
+tth <- function(x, ..., delblanks = TRUE)
 {
-    y <- system(htmltools:::TTH, input = x, intern = TRUE, ignore.stderr = TRUE)
-
-    if(delblanks)
-        y <- y[-grep("^ *$", y)]    
-
-    y
+    y <- system(paste(htmltools:::TTH, tth.control(...)),
+      input = x, intern = TRUE, ignore.stderr = TRUE)
+    if(delblanks) y <- y[-grep("^ *$", y)]
+    return(y)
 }
 
-ttm <- function(x, delblanks = TRUE)
+ttm <- function(x, ..., delblanks = TRUE)
 {
-    y <- system(htmltools:::TTH, input = x, intern = TRUE, ignore.stderr = TRUE)
+    y <- system(paste(htmltools:::TTM, tth.control(...)),
+      input = x, intern = TRUE, ignore.stderr = TRUE)
+    if(delblanks) y <- y[-grep("^ *$", y)]    
+    return(y)
+}
 
-    if(delblanks)
-        y <- y[-grep("^ *$", y)]    
+tth.control <- function(a = FALSE, c = FALSE, d = FALSE, e = 2, f = NULL, g = FALSE,
+  i = FALSE, j = NULL, L = NULL, n = NULL, p = NULL, r = TRUE, t = FALSE, u = FALSE,
+  w = NULL, y = 2, xmakeindxcmd = NULL, v = FALSE)
+{
+  ## collect all arguments
+  rval <- list(a = a, c = c, d = d, e = e, f = f, g = g, i = i, j = j, L = L,
+    n = n, p = p, r = r, t = t, u = u, w = w, y = y, xmakeindxcmd = xmakeindxcmd, v = v)
 
-    y
+  ## sanity checking depending on type
+  if(!is.null(rval[["v"]])) {
+    if(is.numeric(rval[["v"]])) {
+      if(rval[["v"]] > 1L) {
+        rval[["V"]] <- TRUE
+	rval[["v"]] <- NULL
+      } else {
+        rval[["v"]] <- as.logical(rval[["v"]])
+	rval[["V"]] <- NULL
+      }
+    }
+  }
+  for(i in c("a", "c", "d", "g", "i", "r", "t", "u", "v", "V")) {
+    if(!is.null(rval[[i]])) {
+      if(!is.logical(rval[[i]]) | length(rval[[i]]) != 1L) {
+        warning(sprintf("argument %s needs to be a single logical, changed to default", i))
+	rval[[i]] <- NULL
+      }
+    }
+  }
+  for(i in c("e", "f", "j", "n", "w", "y")) {
+    if(!is.null(rval[[i]])) {
+      if(!(is.numeric(rval[[i]]) | is.logical(rval[[i]])) | length(rval[[i]]) != 1L) {
+        warning(sprintf("argument %s needs to be a single numeric, changed to default", i))
+	rval[[i]] <- NULL
+      }
+    }
+  }
+  for(i in c("L", "p", "xmakeindxcmd")) {
+    if(!is.null(rval[[i]])) {
+      if(!is.character(rval[[i]]) | length(rval[[i]]) != 1L) {
+        warning(sprintf("argument %s needs to be a single character, changed to default", i))
+	rval[[i]] <- NULL
+      }
+    }
+  }
+  
+  ## select only non-NULL/FALSE elements
+  rval <- rval[!sapply(rval, is.null)]
+  rval <- rval[!sapply(rval, identical, FALSE)]
+
+  ## collapse to character vector
+  rval <- paste("-", names(rval), ifelse(sapply(rval, isTRUE), "", unlist(rval)),
+    sep = "", collapse = " ")
+  return(rval)
 }
