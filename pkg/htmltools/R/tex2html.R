73652719f606a073548f625e465ecee18ea563ce
leisch (Friedrich Leisch) <friedrich.leisch@r-project.org>
2012-07-29 08:24:29 +0000

    improved package loading
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/exams@215 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/htmltools/R/tex2html.R
+++ pkg/htmltools/R/tex2html.R
@@ -4,22 +4,27 @@ TTH <- TTM <- ""
     
     ## command line options should be arguments of tth(), current
     ## version is proof of concept
-    if(suppressWarnings(require("tth"))) {
-      if(.Platform$OS.type == "windows") {
-        TTH <<- file.path(.find.package("tth"), "libs", .Platform$r_arch, "tth.exe -r -u -e2 -y0")
-        TTM <<- file.path(.find.package("tth"), "libs", .Platform$r_arch, "ttm.exe -r -u -e2 -y0")
-      } else {
-        TTH <<- file.path(.find.package("tth"), "libs", .Platform$r_arch, "tth -r -u -e2 -y0")
-        TTM <<- file.path(.find.package("tth"), "libs", .Platform$r_arch, "ttm -r -u -e2 -y0")
-      }
+    if(length(tthpath <- find.package("tth", quiet=TRUE))) {
+        if(.Platform$OS.type == "windows") {
+            TTH <<- file.path(tthpath, "libs", .Platform$r_arch, "tth.exe -r -u -e2 -y0")
+            TTM <<- file.path(tthpath, "libs", .Platform$r_arch, "ttm.exe -r -u -e2 -y0")
+        } else {
+            TTH <<- file.path(tthpath, "libs", .Platform$r_arch, "tth -r -u -e2 -y0")
+            TTM <<- file.path(tthpath, "libs", .Platform$r_arch, "ttm -r -u -e2 -y0")
+        }
     } else {
-      if(.Platform$OS.type == "windows") {
-        TTH <<- "tth.exe -r -u -e2 -y0"
-        TTM <<- "ttm.exe -r -u -e2 -y0"
-      } else {
-        TTH <<- "tth -r -u -e2 -y0"
-        TTM <<- "ttm -r -u -e2 -y0"
-      }    
+        if(.Platform$OS.type == "windows") {
+            TTH <<- "tth.exe -r -u -e2 -y0"
+            TTM <<- "ttm.exe -r -u -e2 -y0"
+        } else {
+            TTH <<- "tth -r -u -e2 -y0"
+            TTM <<- "ttm -r -u -e2 -y0"
+        }
+        test <- try(system(htmltools:::TTH, input = "Try running tth!",
+                           intern = TRUE, ignore.stderr = TRUE), silent=TRUE)
+        if(inherits(test, "try-error"))
+           stop("Cannot find R package tth or command line untility tth!\n",
+                "Please install suggested R package tth from CRAN.\n")
     }
 }
 
