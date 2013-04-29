6b213ad5310e33cff10fb32e4500fde86fb16f58
freezenik (Nikolaus Umlauf) <nikolaus.umlauf@uibk.ac.at>
2013-04-29 04:47:59 +0000

    updated search files function, should now work, also if more then one match exists within selected directory
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/exams@482 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/exams/R/search_files.R
+++ pkg/exams/R/search_files.R
@@ -1,24 +1,11 @@
-search_files <- function(files, dir = NULL)
+search_files <- function(file, dir = ".")
 {
-  files <- unlist(files)
-
-  stopifnot(is.character(files))
-  dir <- if(is.null(dir)) getwd() else path.expand(dir)
-
-  fpaths <- NULL
-
-  for(f in list.files(dir)) {
-    fp <- file.path(dir, f)
-    if(!file.info(fp)$isdir) {
-      for(j in files) {
-        if(length(fs <- grep(j, fp, fixed = TRUE, value = TRUE))) {
-          fpaths <- c(fpaths, fs)
-        }
-      }
-    } else {
-      fpaths <- c(fpaths, search_files(files, dir = fp))
-    }
-  }
-
-  fpaths
+  lf <- list.files(dir, full.names = TRUE, recursive = TRUE)
+  gr <- lapply(file, grep, lf, fixed = TRUE)
+  ix <- rep(seq_along(file), sapply(gr, length))
+  gr <- unlist(gr)
+  fp <- file.path(dirname(lf[gr]), file[ix])
+  fe <- file.exists(fp)
+  fm <- match(ix[fe], seq_along(file))
+  return(if(length(fm)) fp[fm] else NA)
 }
