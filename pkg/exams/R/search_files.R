097500fdd4199b24953a2eeee4d1dbf746d0be61
azeileis (Achim Zeileis) <Achim.Zeileis@R-project.org>
2013-04-29 07:03:53 +0000

    changed the matching order back again: - only first match is reported - NA is reported if there is no match
    also made matching more strict, now use == instead of grep
    
    
    
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/exams@483 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/exams/R/search_files.R
+++ pkg/exams/R/search_files.R
@@ -1,11 +1,11 @@
 search_files <- function(file, dir = ".")
 {
   lf <- list.files(dir, full.names = TRUE, recursive = TRUE)
-  gr <- lapply(file, grep, lf, fixed = TRUE)
+  bn <- basename(lf)
+  gr <- lapply(file, function(f) which(bn == f))
   ix <- rep(seq_along(file), sapply(gr, length))
   gr <- unlist(gr)
   fp <- file.path(dirname(lf[gr]), file[ix])
   fe <- file.exists(fp)
-  fm <- match(ix[fe], seq_along(file))
-  return(if(length(fm)) fp[fm] else NA)
+  fp[match(seq_along(file), ix[fe])]
 }
