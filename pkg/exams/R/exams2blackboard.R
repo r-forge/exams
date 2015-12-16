0fc77fecdfcee1383824bfb1bb0538b66c40b830
azeileis (Achim Zeileis) <Achim.Zeileis@R-project.org>
2015-12-16 17:30:22 +0000

    omit some old auxiliary functions and tests
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/exams@837 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/exams/R/exams2blackboard.R
+++ pkg/exams/R/exams2blackboard.R
@@ -830,8 +830,6 @@ make_itembody_blackboard <- function(rtiming = FALSE, shuffle = FALSE, rshuffle 
 ## Function to nicely format XML files
 formatXML <- function(files = NULL, dir = NULL, tdir = NULL, overwrite = FALSE)
 {
-  stopifnot(require("XML"))
-  stopifnot(require("tools"))
   xml.string <- add <- FALSE
   if(!is.null(files) & is.null(dir)) {
     if(!all(file.exists(files))) {
@@ -859,7 +857,7 @@ formatXML <- function(files = NULL, dir = NULL, tdir = NULL, overwrite = FALSE)
   for(f in files) {
     setwd(dirname(f))
     fbn <- basename(f)
-    xml <- try(xmlTreeParse(fbn)$doc$children[[1]], silent = TRUE)
+    xml <- try(XML::xmlTreeParse(fbn)$doc$children[[1]], silent = TRUE)
     if(inherits(xml, "try-error")) warning(paste("could not format file:", f))
     sink(file.path(tdir, fbn))
     print(xml)
@@ -871,163 +869,4 @@ formatXML <- function(files = NULL, dir = NULL, tdir = NULL, overwrite = FALSE)
     return(file.copy(file.path(tdir, fbn), file.path(dir, fn), overwrite = overwrite))
   else
     return(readLines(file.path(tdir, fbn)))
-}
-
-
-###############################
-## OLD TESTING SECTION NIELS ##
-###############################
-#function for generating random identifiers
-randomstring<-function(a=1,n=31){
-z<-vector(length=a)
-  for (h in 1:a){
-  x<-"n"
-  for (i in 1:n){
-  if (runif(1)>0.5){
-  y<-round(runif(1,-.5,9.5))
-      }
-      else {y<-letters[round(runif(1,.5,24.5))]
-      }
-  x<-paste(x,y, sep = "")
-    }
-  z[h]<-x  }
-  z
-}
-
-
-
-#function to write exam items into a Blackboard format
-mc2bb<-function(exam.object,inst="Make these items",correct=1,naam="tryout",desc="MC Questions",path="C:\\BB\\"){
-
-
-  eo<-exam.object
-  nritems<-length(eo)
-
-  iden<-as.numeric(format(Sys.time(), "%H%M%S"))+100000
-  tijd<-format(Sys.time(), "%Y-%m-%d-%H%M")
-  dir.create(path, showWarnings = FALSE)
-  dir.create(paste(path,tijd,sep=""))
-  dir.create(paste(path,tijd,"\\res00002",sep=""))
-  #write info to .bb-package-info, res00001.dat, res00003.dat
-  cat(paste("<?xml version=\"1.0\" encoding=\"UTF-8\"?>","\n<CATEGORIES><CATEGORY id=\"_3728_1\"><TITLE>\"OMII-A\"</TITLE><TYPE>category</TYPE><COURSEID value=\"_38547_1\"/></CATEGORY></CATEGORIES>"),sep="",file=paste(path,tijd,"\\res00001.dat",sep=""))
-  cat(paste("<?xml version=\"1.0\" encoding=\"UTF-8\"?>","\n<ASSESSMENTCREATIONSETTINGS><ASSESSMENTCREATIONSETTING id=\"_22705_1\"><QTIASSESSMENTID value=\"_",iden,"_1\"/><ANSWERFEEDBACKENABLED>false</ANSWERFEEDBACKENABLED><QUESTIONATTACHMENTSENABLED>true</QUESTIONATTACHMENTSENABLED><ANSWERATTACHMENTSENABLED>false</ANSWERATTACHMENTSENABLED><QUESTIONMETADATAENABLED>false</QUESTIONMETADATAENABLED><DEFAULTPOINTVALUEENABLED>false</DEFAULTPOINTVALUEENABLED><DEFAULTPOINTVALUE>10.0</DEFAULTPOINTVALUE><ANSWERPARTIALCREDITENABLED>false</ANSWERPARTIALCREDITENABLED><ANSWERRANDOMORDERENABLED>true</ANSWERRANDOMORDERENABLED><ANSWERORIENTATIONENABLED>true</ANSWERORIENTATIONENABLED><ANSWERNUMBEROPTIONSENABLED>true</ANSWERNUMBEROPTIONSENABLED></ASSESSMENTCREATIONSETTING></ASSESSMENTCREATIONSETTINGS>",sep=""),file=paste(path,tijd,"\\res00003.dat",sep=""),sep="")
-  cat(paste("#CxPackageInfo Property File","\n#",format(Sys.time(), "%A %b %d %X %Y"),"\njava.class.path=\\:/usr/local/blackboard/apps/tomcat/bin/bootstrap.jar\\:/usr/local/blackboard/apps/tomcat/bin/commons-logging-api.jar\ncx.config.course.id=FPP_812005_2007_1\njava.vendor=Sun Microsystems Inc.\nos.name=Linux\nos.arch=i386\njava.home=/usr/java/jdk1.5.0_07/jre\ncx.package.info.version=6.0\ndb.product.name=Oracle\nos.version=2.6.9-67.ELsmp\ncx.config.full.value=CxConfig{operation\\=EXPORT, courseid\\=FPP_812005_2007_1, package\\=/usr/local/blackboard/content/vi/bb_bb60/sessions/1/3/6/0/4/8/5/7/2/session/Pool_ExportFile_FPP_812005_2007_1_",naam,".zip, logs\\=[Log{name\\=/usr/local/blackboard/content/vi/bb_bb60/sessions/1/3/6/0/4/8/5/7/2/session/Pool_ExportFile_FPP_812005_2007_1_",naam,".log, verbosity\\=default}, Log{name\\=stdout, verbosity\\=default}], area_inclusions\\=[], area_exclusions\\=[ALL], object_inclusions\\={POOL\\=[_",iden,"_1]}, object_exclusions\\={}, features\\={CreateOrg\\=false, Bb5LinkToBrokenImageFix\\=true}}\ncx.config.package.name=/usr/local/blackboard/content/vi/bb_bb60/sessions/1/3/6/0/4/8/5/7/2/session/Pool_ExportFile_FPP_812005_2007_1_",naam,".zip\ndb.product.version=Oracle Database 10g Enterprise Edition Release 10.2.0.3.0 - Production\nWith the Partitioning, OLAP and Data Mining options\ndb.driver.name=Oracle JDBC driver\ncx.config.operation=blackboard.apps.cx.CxConfig$Operation\\:EXPORT\njava.version=1.5.0_07\ndb.driver.version=10.2.0.1.0\njava.default.locale=en_US\napp.release.number=7.3.216.0\ncx.config.logs=[Log{name\\=/usr/local/blackboard/content/vi/bb_bb60/sessions/1/3/6/0/4/8/5/7/2/session/Pool_ExportFile_FPP_812005_2007_1_",naam,".log, verbosity\\=default}, Log{name\\=stdout, verbosity\\=default}]",sep=""),file=paste("C:\\BB\\",tijd,"\\.bb-package-info",sep=""))
-
-  txta1<-paste("<?xml version=\"1.0\" encoding=\"UTF-8\"?><questestinterop><assessment title=\"")#set name
-  txta2<-paste("\"><assessmentmetadata><bbmd_asi_object_id>",paste("_",iden,"_",1,sep=""),sep="")#unique number
-  txta3<-paste("</bbmd_asi_object_id><bbmd_asitype>Assessment</bbmd_asitype><bbmd_assessmenttype>Pool</bbmd_assessmenttype><bbmd_sectiontype>Subsection</bbmd_sectiontype><bbmd_questiontype>Multiple Choice</bbmd_questiontype><bbmd_is_from_cartridge>false</bbmd_is_from_cartridge><bbmd_numbertype>none</bbmd_numbertype><bbmd_partialcredit/><bbmd_orientationtype>vertical</bbmd_orientationtype><bbmd_is_extracredit>false</bbmd_is_extracredit><qmd_absolutescore_max>0.0</qmd_absolutescore_max><qmd_weighting>0.0</qmd_weighting></assessmentmetadata><rubric view=\"All\"><flow_mat class=\"Block\"><material><mat_extension><mat_formattedtext type=\"HTML\"><![CDATA[")#Instructions
-  txta4<-paste("<br />]]></mat_formattedtext></mat_extension></material></flow_mat></rubric><presentation_material><flow_mat class=\"Block\"><material><mat_extension><mat_formattedtext type=\"HTML\"><![CDATA[")#Short description
-  txta5<-paste("<br />]]></mat_formattedtext></mat_extension></material></flow_mat></presentation_material><section><sectionmetadata><bbmd_asi_object_id>",paste("_",iden+1,"_",1,sep=""),"</bbmd_asi_object_id><bbmd_asitype>Section</bbmd_asitype><bbmd_assessmenttype>Pool</bbmd_assessmenttype><bbmd_sectiontype>Subsection</bbmd_sectiontype><bbmd_questiontype>Multiple Choice</bbmd_questiontype><bbmd_is_from_cartridge>false</bbmd_is_from_cartridge><bbmd_numbertype>none</bbmd_numbertype><bbmd_partialcredit/><bbmd_orientationtype>vertical</bbmd_orientationtype><bbmd_is_extracredit>false</bbmd_is_extracredit><qmd_absolutescore_max>0.0</qmd_absolutescore_max><qmd_weighting>0.0</qmd_weighting></sectionmetadata>",sep="")#Begin Item part
-   txta<-paste(txta1,naam,txta2,txta3,inst,txta4,desc,txta5,sep="")
-
-  #text for imsmanifest.xml file
-  txtd1<-paste("<?xml version=\"1.0\" encoding=\"UTF-8\"?>","\n<manifest identifier=\"man00001\" xmlns:bb=\"http://www.blackboard.com/content-packaging/\"><organizations default=\"toc00001\"><organization identifier=\"toc00001\"/></organizations><resources><resource bb:file=\"res00001.dat\" bb:title=\"Categories\" identifier=\"res00001\" type=\"course/x-bb-category\" xml:base=\"res00001\"/><resource bb:file=\"res00002.dat\" bb:title=\"",naam,"\" identifier=\"res00002\" type=\"assessment/x-bb-qti-pool\" xml:base=\"res00002\">",sep="")
-  txtd2<-paste("</resource><resource bb:file=\"res00003.dat\" bb:title=\"Assessment Creation Settings\" identifier=\"res00003\" type=\"course/x-bb-courseassessmentcreationsettings\" xml:base=\"res00003\"/></resources></manifest>")
-
-  #empty text string
-  txt<-""
-
-
-  for (i in 1:nritems){
-
-    item<-eo[[i]][[1]]
-    #I have to make single string of whole body because if I don't it just takes first element of character string
-    #gives bad tables though (writeLines may be better)
-    item$body<-Reduce(paste0,item$question)
-    item$answers<-Reduce(paste0,item$solutionlist)
-
-
-
-    #begin item
-    txtb1<-paste("<item maxattempts=\"0\"><itemmetadata><bbmd_asi_object_id>",paste("_",iden+1+i,"_",1,sep=""),"</bbmd_asi_object_id><bbmd_asitype>Item</bbmd_asitype><bbmd_assessmenttype>Pool</bbmd_assessmenttype><bbmd_sectiontype>Subsection</bbmd_sectiontype><bbmd_questiontype>Multiple Choice</bbmd_questiontype><bbmd_is_from_cartridge>false</bbmd_is_from_cartridge><bbmd_numbertype>letter_lower</bbmd_numbertype><bbmd_partialcredit>false</bbmd_partialcredit><bbmd_orientationtype>vertical</bbmd_orientationtype><bbmd_is_extracredit>false</bbmd_is_extracredit><qmd_absolutescore_max>0.0</qmd_absolutescore_max><qmd_weighting>0.0</qmd_weighting></itemmetadata><presentation><flow class=\"Block\"><flow class=\"QUESTION_BLOCK\"><flow class=\"FORMATTED_TEXT_BLOCK\"><material><mat_extension><mat_formattedtext type=\"HTML\"><![CDATA[",sep="")
-    txtb2<-paste("]]></mat_formattedtext></mat_extension></material></flow></flow>")
-
-    #start response block
-
-    txtb<-paste(txtb1,item$body,txtb2,sep="")
-
-
-    l<-length(item$questionlist)
-    ids<-randomstring(a=l)
-    txtc1<-paste("<flow class=\"RESPONSE_BLOCK\"><response_lid ident=\"response\" rcardinality=\"Single\" rtiming=\"No\"><render_choice maxnumber=\"0\" minnumber=\"0\" shuffle=\"Yes\">")
-    txtc2<-""
-    for (i in 1:l){
-    txt01<-paste("<flow_label class=\"Block\"><response_label ident=\"",ids[i],"\" rarea=\"Ellipse\" rrange=\"Exact\" shuffle=\"Yes\"><flow_mat class=\"FORMATTED_TEXT_BLOCK\"><material><mat_extension><mat_formattedtext type=\"HTML\"><![CDATA[",item$questionlist[i],"<br />]]></mat_formattedtext></mat_extension></material></flow_mat></response_label></flow_label>",sep="")
-    txtc2<-paste(txtc2,txt01,sep="")}
-    txtc3<-paste("</render_choice></response_lid></flow></flow></presentation>")
-    txtc4<-paste("<resprocessing scoremodel=\"SumOfScores\"><outcomes><decvar defaultval=\"0.0\" minvalue=\"0.0\" varname=\"SCORE\" vartype=\"Decimal\"/></outcomes><respcondition title=\"correct\"><conditionvar><varequal case=\"No\" respident=\"response\">",ids[correct],"</varequal></conditionvar><setvar action=\"Set\" variablename=\"SCORE\">SCORE.max</setvar><displayfeedback feedbacktype=\"Response\" linkrefid=\"correct\"/></respcondition><respcondition title=\"incorrect\"><conditionvar><other/></conditionvar><setvar action=\"Set\" variablename=\"SCORE\">0.0</setvar><displayfeedback feedbacktype=\"Response\" linkrefid=\"incorrect\"/></respcondition>",sep="")
-    txtc5<-""
-    for (i in 1:l){
-    txt01<-paste("<respcondition><conditionvar><varequal case=\"No\" respident=\"",ids[i],"\"/></conditionvar><setvar action=\"Set\" variablename=\"SCORE\">0</setvar><displayfeedback feedbacktype=\"Response\" linkrefid=\"",ids[i],"\"/></respcondition>",sep="")
-    txtc5<-paste(txtc5,txt01,sep="")}
-    txtc6<-paste("</resprocessing><itemfeedback ident=\"correct\" view=\"All\"><flow_mat class=\"Block\"><flow_mat class=\"FORMATTED_TEXT_BLOCK\"><material><mat_extension><mat_formattedtext type=\"HTML\"><![CDATA[Goed!<br />",item$answers,"<br />]]></mat_formattedtext></mat_extension></material></flow_mat></flow_mat></itemfeedback><itemfeedback ident=\"incorrect\" view=\"All\"><flow_mat class=\"Block\"><flow_mat class=\"FORMATTED_TEXT_BLOCK\"><material><mat_extension><mat_formattedtext type=\"HTML\"><![CDATA[Fout <br />",item$answers,"<br />]]></mat_formattedtext></mat_extension></material></flow_mat></flow_mat></itemfeedback>")
-    txtc7<-""
-    for (i in 1:l){
-    txt01<-paste("<itemfeedback ident=\"",ids[i],"\" view=\"All\"><solution feedbackstyle=\"Complete\" view=\"All\"><solutionmaterial><flow_mat class=\"Block\"><flow_mat class=\"FORMATTED_TEXT_BLOCK\"><material><mat_extension><mat_formattedtext type=\"HTML\"/></mat_extension></material></flow_mat></flow_mat></solutionmaterial></solution></itemfeedback>",sep="")
-    txtc7<-paste(txtc7,txt01,sep="")}
-    txtc8<-paste("</item>")
-     txtc<-paste(txtc1,txtc2,txtc3,txtc4,txtc5,txtc6,txtc7,txtc8,sep="")
-
-    txt<-paste(txt,txtb,txtc)
-
-
-    }#end of loop
-
-  #write to imsmanifest.xml
-  cat(paste(txtd1,txtd2,sep=""),file=paste(path,tijd,"\\imsmanifest.xml",sep=""))
-
-  #write to res00002.dat
-  cat(txta,txt,paste("</section></assessment></questestinterop>"),sep="",file=paste(path,tijd,"\\res00002.dat",sep=""))
-
-  test_dir<-paste(path,tijd,sep="")
-  owd <- getwd()
-  setwd(test_dir)
-  zip(zipfile = zipname <- paste(test_dir, "zip", sep = "."),
-            files = list.files(test_dir))
-  setwd(owd)
-
-}
-
-
-## TESTING.
-if(FALSE) {
-options(SweaveSyntax = SweaveSyntaxNoweb)
-library(exams)
-odir <- "C:\\BB"
-#exams2html("anova.Rnw",dir=odir)
-
-## to produce the building blocks for the same exerce:
-## set up a LaTeX-to-HTML converter based on tth and base64enc
-htmltrafo <- make_exercise_transform_html()
-
-## set the options for calling Sweave
-args <- list(quiet = TRUE, pdf = FALSE, png = TRUE, resolution = 100,
-  height = 4, width = 4)
-
-## generate 1 exam with 1 exercise (anova.Rnw) in HTML+MathML
-set.seed(1090)
-exm <- xexams("boxplots", n = 10,
-  driver = list(sweave = args, read = NULL, transform = htmltrafo))
-
-## question (without multiple choice statements)
-exm[[1]][[1]]$question[-11]
-
-## multiple choice statements
-exm[[1]][[1]]$questionlist
-
-## correct solution text
-exm[[1]][[1]]$solution
-
-## and explanations corresponding to multiple choice statements
-exm[[1]][[1]]$solutionlist
-
-#In the $question I excluded line 11 because this contains the PNG image (in Base64 encoding). This is too long to usefully print on the screen...
-
-#To see what the HTML text looks like in a browser you can also do
-
-#exams:::show.html(exm[[1]][[1]]$question)
-
-##source("D:/My Documents/Onderwijs/QTI.XML/exams2BB.R")
-
-mc2bb(exm)
 }
