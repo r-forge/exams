36aaa314fc50014a645ce7a6d975e84c97a9d1a1
dernst (Dominik Ernst) <dominik.ernst@boku.ac.at>
2016-05-31 15:54:30 +0000

    added moodle free text questions
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/exams@902 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/exams/R/exams2moodle.R
+++ pkg/exams/R/exams2moodle.R
@@ -211,7 +211,9 @@ make_question_moodle <-
 make_question_moodle23 <- function(name = NULL, solution = TRUE, shuffle = FALSE, penalty = 0,
   answernumbering = "abc", usecase = FALSE, cloze_mchoice_display = "MULTICHOICE",
   truefalse = c("True", "False"), enumerate = TRUE, abstention = NULL,
-  eval = list(partial = TRUE, negative = FALSE, rule = "false2"))
+  eval = list(partial = TRUE, negative = FALSE, rule = "false2"),
+  essay = NULL)
+
 {
   function(x) {
     ## how many points?
@@ -232,6 +234,10 @@ make_question_moodle23 <- function(name = NULL, solution = TRUE, shuffle = FALSE
       "cloze" = "cloze",
       "string" = "shortanswer"
     )
+
+    if(type == "shortanswer" && (isTRUE(x$metainfo$essay) || isTRUE(essay))) {
+        type <- "essay"
+    }
 
     ## question name
     if(is.null(name)) name <- x$metainfo$name
@@ -338,6 +344,40 @@ make_question_moodle23 <- function(name = NULL, solution = TRUE, shuffle = FALSE
         '</answer>'
       )
     }
+
+    ## string/essay questions
+    if(type == "essay") {
+        essay_opts <- list(format="plain", required=TRUE, fieldlines=5L,
+            attachments=0L, attachmentsrequired=FALSE)
+
+        if(!is.list(essay)) {
+            essay <- list()
+        }
+
+        for(i in names(essay_opts)) {
+            vn <- paste0("essay_", i)
+            value <- x$metainfo[[vn]]
+
+            if(!is.null(value)) {
+                essay_opts[[i]] <- value
+            }
+        }
+
+        for(i in names(essay_opts)) {
+            if(!is.null(essay[[i]])) essay_opts[[i]] <- essay[[i]]
+        }
+
+        txt <- paste0(
+            "<responseformat>", essay_opts$format, "</responseformat>\n",
+            "<responserequired>", as.integer(essay_opts$required), "</responserequired>\n",
+            "<responsefieldlines>", essay_opts$fieldlines, "</responsefieldlines>\n",
+            "<attachments>", essay_opts$attachments, "</attachments>\n",
+            "<attachmentsrequired>", as.integer(essay_opts$attachmentsrequired), "</attachmentsrequired>\n"
+        )
+
+        xml <- c(xml, txt)
+    }
+
 
     ## cloze type questions
     if(type == "cloze") {
