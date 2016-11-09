175935206dc10d3f8badbd91df94b9a1716f7afb
freezenik (Nikolaus Umlauf) <nikolaus.umlauf@uibk.ac.at>
2016-11-09 05:40:13 +0000

    added more verbose info
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/exams@945 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/exams/R/stresstests.R
+++ pkg/exams/R/stresstests.R
@@ -4,6 +4,8 @@ stresstest_exercise <- function(file, n = 100, plot = TRUE, verbose = TRUE, seed
   sq <- objects <- vector("list", length = n)
   seeds <- if(!is.null(seeds)) rep(seeds, length.out = n) else 1:n
   times <- rep(0, n)
+  if(verbose)
+    cat("testing file", file, "\n")
   for(i in 1:n) {
     set.seed(seeds[i])
     if(verbose) cat(seeds[i])
