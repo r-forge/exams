f9f1e274689f1b76f26a84b32fd1ccaf20145f6c
azeileis (Achim Zeileis) <Achim.Zeileis@R-project.org>
2016-12-02 14:36:42 +0000

    - added exams2ilias() based on Mirko's draft,   essentially just calls exams2qti12() with some custom options
    - custom ilias.xml template
    
    - added xmlcollapse = FALSE option for exams2qti12(),
      can collapse line breaks in XML code,
      needed for exams2ilias()
    
    
    
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/exams@964 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ pkg/exams/R/exams2ilias.R
@@ -0,0 +1,45 @@
+exams2ilias <- function(file, n = 1L, nsamp = NULL, dir = ".",
+  name = NULL, quiet = TRUE, edir = NULL, tdir = NULL, sdir = NULL, verbose = FALSE,
+  resolution = 100, width = 4, height = 4, svg = FALSE, encoding  = "",
+  num = list(fix_num = FALSE, minvalue = NA),
+  mchoice = list(maxchars = c(3, NA, 3), minvalue = NA),
+  schoice = mchoice, string = NULL, cloze = NULL,
+  template = "ilias",
+  duration = NULL, stitle = "Exercise", ititle = "Question",
+  adescription = "Please solve the following exercises.",
+  sdescription = "Please answer the following question.", 
+  maxattempts = 1, cutvalue = 0, solutionswitch = TRUE, zip = FALSE,
+  points = NULL, eval = list(partial = TRUE, negative = FALSE),
+  converter = "pandoc-mathjax", ...)
+{
+  ## assure a certain processing of items for Ilias
+  if(is.null(num)) {
+    num <- list(fix_num = FALSE, minvalue = NA)
+  } else {
+    num$fix_num <- FALSE
+    num$minvalue <- NA
+  }
+  if(is.null(mchoice)) {
+    mchoice <- list(maxchars = c(3, NA, 3), minvalue = NA)
+  } else {
+    mchoice$maxchars <- c(3, NA, 3)
+    mchoice$minvalue <- NA
+  }
+  if(is.null(schoice)) {
+    schoice <- list(maxchars = c(3, NA, 3), minvalue = NA)  
+  } else {
+    schoice$maxchars <- c(3, NA, 3)
+    schoice$minvalue <- NA
+  }
+
+  ## simply call exams2qti12 with custom template (and XML processing)
+  exams2qti12(file = file, n = n, nsamp = nsamp, dir = dir,
+    name = name, quiet = quiet, edir = edir, tdir = tdir, sdir = sdir, verbose = verbose,
+    resolution = resolution, width = width, height = height, svg = svg, encoding  = encoding,
+    num = num, mchoice = mchoice, schoice = schoice, string = string, cloze = cloze,
+    template = template,
+    duration = duration, stitle = stitle, ititle = ititle,
+    adescription = adescription, sdescription = sdescription, 
+    maxattempts = maxattempts, cutvalue = cutvalue, solutionswitch = solutionswitch, zip = zip,
+    points = points, eval = eval, converter = converter, xmlcollapse = TRUE, ...)
+}
