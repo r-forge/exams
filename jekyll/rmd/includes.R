c06347b6f7b44d19d49316a7d8c926c8c8014bc0
azeileis (Achim Zeileis) <Achim.Zeileis@R-project.org>
2017-08-04 11:34:36 +0000

    more work on first exams2nops post
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/exams@1081 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- jekyll/rmd/includes.R
+++ jekyll/rmd/includes.R
@@ -1,6 +1,6 @@
-knit_to_jekyll <- function(file, ...) {
+include_rmd <- function(file, ...) {
   knitr::opts_chunk$set(dev = "svg",
     fig.height = 5, fig.width = 6, dpi = 100, ...,
     fig.path = "../../assets/svg/", knitr::render_jekyll("prettify"))
-  knitr::knit(file, quiet = TRUE, encoding = "UTF-8")
+  knitr::knit(input = file, quiet = TRUE, encoding = "UTF-8")
 }
