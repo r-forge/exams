3018fcc18bbfadab055a72caa5b69af4c2532f09
azeileis (Achim Zeileis) <Achim.Zeileis@R-project.org>
2018-04-06 00:02:04 +0000

    improve include_tikz to optionally return markup for "tex", "markdown", or "none". In the latter case just the file names are returned.
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/exams@1450 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/exams/R/include_tikz.R
+++ pkg/exams/R/include_tikz.R
@@ -1,10 +1,18 @@
-include_tikz <- function(tikz, name = "tikzpicture", format = NULL, library = NULL, width = NULL, ...)
+include_tikz <- function(tikz, name = "tikzpicture", format = NULL,
+  library = NULL, width = NULL, markup = "tex", ...)
 {
   ## defaults
   if(is.null(format)) format <- "png"
   if(is.null(library)) library <- TRUE
   if(is.null(width)) width <- ""
   if(nchar(width) > 1L && substr(width, 1L, 1L) != "[") width <- sprintf("[width=%s]", width)
+
+  ## output markup
+  markup <- match.arg(markup, c("tex", "markdown", "none"))
+  if(format == "tex" && markup != "tex") {
+    warning("'tex' format only supported within 'tex' markup, changed to 'png'")
+    format <- "png"
+  }
   
   ## add {tikzpicture} if necessary
   if(!(any(grepl("begin{tikzpicture}", tikz, fixed = TRUE)) &
@@ -14,10 +22,20 @@ include_tikz <- function(tikz, name = "tikzpicture", format = NULL, library = NU
 
   if(format == "tex") {
     writeLines(tikz)
+    invisible(tikz)
   } else {
     ## call tex2image
     tex2image(tikz, name = name, dir = ".", format = format, tikz = library, show = FALSE, ...)
-    writeLines(sprintf("\\includegraphics%s{%s}", width, paste(name, format, sep = ".")))
+    fig <- paste(name, format, sep = ".")
+    if(markup == "tex") {
+      writeLines(sprintf("\\includegraphics%s{%s}", width, fig))
+      invisible(fig)
+    } else if(markup == "markdown") {
+      writeLines(sprintf("![](%s)", fig))
+      invisible(fig)
+    } else {
+      return(fig)
+    }
   }
 }
 
