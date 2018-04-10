ea5200ddb95a73f9cb1a880ade4e5c50715d0450
freezenik (Nikolaus Umlauf) <nikolaus.umlauf@uibk.ac.at>
2018-04-10 12:46:24 +0000

    vQTI for version 2.1 now
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/exams@1460 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/exams/R/xmltools.R
+++ pkg/exams/R/xmltools.R
@@ -1,11 +1,12 @@
 ## Valid QTI xml code, uses xmllint
-validQTI <- function(file = "~/tmp/qti12.zip", dtd = NULL, tdir = "~/tmp/dtd")
+validQTI <- function(file = "~/tmp/qti12.zip", dtd = "~/dtd_v2p1.xsd", tdir = "~/tmp/dtd")
 {
   owd <- getwd()
   on.exit(setwd(owd))
 
   if(is.null(dtd))
     dtd <- "http://www.imsglobal.org/question/qtiv1p2/qtiasifulldtd/ims_qtiasiv1p2.dtd"
+  ## "https://github.com/oat-sa/qti-sdk/raw/master/src/qtism/data/storage/xml/schemes/qtiv2p1/imsqti_v2p1.xsd"
   dtd2 <- readLines(dtd)
   if(is.null(tdir))
     tdir <- tempfile()
@@ -17,33 +18,36 @@ validQTI <- function(file = "~/tmp/qti12.zip", dtd = NULL, tdir = "~/tmp/dtd")
 
   setwd(tdir)
 
+  dtd_name <- paste0(file_path_sans_ext(basename(dtd)), ".dtd")
+  writeLines(dtd2, file.path(tdir, dtd_name))
+
   if(file_ext(file) == "zip") {
     unzip(file)
   }
-  xml <- grep(".xml", dir(tdir), value = TRUE)
+  xml <- grep(".xml", dir(tdir), value = TRUE, fixed = TRUE)
   if(!length(xml)) stop("no xml file found for validation!")
-  xml2 <- readLines(xml)
-  i <- grep(".dtd", xml2, fixed = TRUE)
-  dtd_name <- strsplit(xml2[i], '"', fixed = TRUE)[[1]][2]
-  writeLines(dtd2, file.path(tdir, dtd_name))
+  log <- NULL
+  for(j in seq_along(xml)) {
+    cmd <- paste('xmllint --schema  ~/Git/qti-sdk/src/qtism/data/storage/xml/schemes/qtiv2p1/imsqti_v2p1.xsd', xml[j], " > log.txt")
+    log <- c(log, system(cmd))
+  }
 
-  cmd <- paste('xmllint --valid', xml, " > /dev/null")
-  system(cmd)
-
-  invisible(NULL)
+  return(log)
 }
 
 
 ## Small wrapper to check xml code
-vQTI <- function(file = c("boxplots", "tstat", "boxhist", "superheroes"),
-  dir = "~/tmp", dtd = NULL, ...)
+vQTI <- function(file = c("boxplots", "tstat", "ttest", "confint",
+  "regression", "anova", "scatterplot", "boxhist", "relfreq", "boxhist2",   
+  "fourfold2"), dir = "~/tmp", dtd = "~/dtd_v2p1.xsd", ...)
 {
   if(is.null(dir)) {
     dir.create(dir <- tempfile())
     on.exit(unlink(dir))
   }
 
-  exams2qti12(file, dir = dir, name = "vQTI", ...)
+  exams2qti21(file, dir = dir, name = "vQTI", ...)
 
   validQTI(file = file.path(dir, "vQTI.zip"), dtd = dtd)
 }
+
