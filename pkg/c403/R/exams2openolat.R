f2884d2c38794bf6ca0fe6e0274a56b1aa5d3224
azeileis (Achim Zeileis) <Achim.Zeileis@R-project.org>
2018-05-21 00:06:20 +0000

    added convenience interface c403::exams2openolat to exams::exams2openolat with German descriptions and saving .rds file
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/exams@1526 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ pkg/c403/R/exams2openolat.R
@@ -0,0 +1,18 @@
+exams2openolat <- function(file, n = 1L, dir = ".", name = "olattest", maxattempts = 1,
+  cutvalue = 1000, solutionswitch = FALSE, qti = "1.2",
+  stitle = "Aufgabe", ititle = "Frage",
+  adescription = "Bitte bearbeiten Sie folgende Aufgaben.",
+  sdescription = "Bitte beantworten Sie folgende Frage.",
+  eval = list(partial = FALSE, negative = FALSE),
+  ...)
+{
+  ## call exams::exams2openolat
+  rval <- exams::exams2openolat(file, n = n, dir = dir, name = name, maxattempts = maxattempts,
+    cutvalue = cutvalue, solutionswitch = solutionswitch, qti = qti,
+    stitle = stitle, ititle = ititle, adescription = adescription,
+    sdescription = sdescription, eval = eval, ...)
+
+  ## save exams list generated
+  saveRDS(rval, file = file.path(dir, paste(name, ".rds", sep = "")))
+  invisible(rval)
+}
