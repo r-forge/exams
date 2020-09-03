89b4e922da8e8ef7df6950bc12b7c815c8e5d74a
kenjisato (Kenji Sato) <kenji@kenjisato.jp>
2020-09-03 08:53:12 +0000

    Update nops_eval_write_template
    1. It now allows for non-HTML templates. `return_scan` (default: FALSE) option is added for the case where scans cannot be embedded.  
    2. It is now possible to produce a directory containing individual reports. When the new`zip` option is FALSE, the function does not compress the reports.
    3. whisker templates can use DCF list variable for values defined in .dcf files. 
    
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/exams@1941 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/exams/R/nops_eval_write_template.R
+++ pkg/exams/R/nops_eval_write_template.R
@@ -1,34 +1,68 @@
+## format multiple choice solutions
+## I want this function in exams_eval.R.
+## format_mchoice(c("11100",  "11110")) -> c("abc__", "abcd_")
+format_mchoice <- function(x) {
+  mchoice2print <- function(x) {
+    paste(ifelse(x, letters[1L:5L], rep("_", 5L)),  collapse = "")
+  }
+  sapply(strsplit(x, ""),
+         function(z) mchoice2print(as.logical(as.numeric(z))))
+}
+
+
+.nops_eval_choose_template <- function(template = NULL, file) {
+  
+  if (!is.null(template)) {
+    ext <- tools::file_ext(template)
+    template <- tools::file_path_as_absolute(template)
+  } else {
+    ext <- tools::file_ext(file)
+    ext <- if (nchar(ext) > 0L) ext else "html"
+    basename <- paste0("eval.", ext)
+    template <- system.file(file.path("nops", basename), package = "exams")
+    if (nchar(template) == 0L) {
+      stop("No default tamplate with extenstion, ", ext, ".")
+    }
+  }
+  list(template = readLines(template),
+       report_name = paste0(tools::file_path_sans_ext(basename(file)), ".", ext))
+}
+
+
 nops_eval_write_template <- function(results = "nops_eval.csv",
                                      file = "exam_eval",
                                      dir = ".", language = "en",
                                      template = NULL, encoding = "UTF-8", 
-                                     converter = NULL, ...) {
+                                     converter = NULL, zip = TRUE, 
+                                     return_scan = FALSE, ...) {
 
   stopifnot(requireNamespace("base64enc"))
   stopifnot(requireNamespace("whisker"))
 
-  ## output file names
-  out_file <- paste0(tools::file_path_sans_ext(basename(file)), ".html")
-  out_zip <- paste0(tools::file_path_sans_ext(basename(results)), ".zip")
+  ## output file
+  out_name <- tools::file_path_sans_ext(basename(results))
   
-  ## user lists
+  #' Reading template
+  #' If `file` argument has an extension, ext, and `template` argument is NULL,  
+  #' then the corresponding "/nops/eval.ext" will be searched for. If `file` 
+  #' argument does not have an extension and `template` is NULL, then the 
+  #' default HTML template, "/nops/eval.html", is used. If `template` is 
+  #' specified, the extension of `file` argument is ignored and the extension 
+  #' of the template is used.
+  tmpl <- .nops_eval_choose_template(template, file)
+  template <- tmpl$template
+  report_name <- tmpl$report_name
+  
+  ## Read results
   results <- utils::read.csv2(results, colClasses = "character")
   names(results)[1:3] <- c("registration", "name", "id")
   rownames(results) <- results$registration
   has_mark <- "mark" %in% names(results)
   
-  ## dimensions
+  ### Dimensions
   m <- length(grep("answer.", colnames(results), fixed = TRUE))
   n <- nrow(results)
   
-  ## format multiple choice solutions
-  format_mchoice <- function(x) {
-    mchoice2print <- function(x) {
-      paste(ifelse(x, letters[1L:5L], rep("_", 5L)),  collapse = "")
-    }
-    sapply(strsplit(x, ""),
-           function(z) mchoice2print(as.logical(as.numeric(z))))
-  }
   for (i in as.vector(outer(c("answer", "solution"), 1L:m, paste, sep = "."))) {
     results[[i]] <- format_mchoice(results[[i]])
   }
@@ -47,46 +81,28 @@ nops_eval_write_template <- function(results = "nops_eval.csv",
   if (language == "") {
     language <- system.file(file.path("nops", "en.dcf"), package = "exams")
   }
-  lang <- nops_language(language, converter = converter)
-  substr(lang$Points, 1L, 1L) <- toupper(substr(lang$Points, 1L, 1L))
-  if (!is.null(lang$PointSum)) {
-    lang$Points <- lang$PointSum
+  DCF <- nops_language(language, converter = converter)
+  substr(DCF$Points, 1L, 1L) <- toupper(substr(DCF$Points, 1L, 1L))
+  if (!is.null(DCF$PointSum)) {
+    DCF$Points <- DCF$PointSum
   }
-
-  ## HTML template
-  template <- if (!is.null(template)) {
-    tools::file_path_as_absolute(template)
-  } else {
-    system.file(file.path("nops", "eval.html"), package = "exams")
-  }
-  template <- readLines(template)
-
-  commonDat <- list(
+  
+  commonData <- list(
     language = basename(tools::file_path_sans_ext(language)),
     encoding = encoding,
     has_mark = has_mark,
-    LANG_ExamResults = lang$ExamResults,
-    LANG_Name = lang$Name,
-    LANG_RegistrationNumber = lang$RegistrationNumber,
-    LANG_DocumentID = lang$DocumentID,
-    LANG_Points = lang$Points,
-    LANG_Evaluation = lang$Evaluation,
-    LANG_Mark = lang$Mark,
-    LANG_Question = lang$Question,
-    LANG_GivenAnswer = lang$GivenAnswer,
-    LANG_CorrectAnswer = lang$CorrectAnswer,
-    LANG_ExamSheet = lang$ExamSheet
+    DCF = DCF
   )
   checkClasses <- c("negative", "neutral", "positive", "full")
 
   ## directories
-  odir <- getwd()
+  work_dir <- getwd()
   dir.create(temp_dir <- tempfile())
-  setwd(temp_dir)
-  on.exit(setwd(odir))
 
+  ## Write out exam reports.
   for (i in 1L:nrow(results)) {
-    dat <- commonDat
+    ## Copy information common to all students.
+    dat <- commonData
 
     id <- rownames(results)[i]
     ac <- results[id, "id"]
@@ -113,19 +129,38 @@ nops_eval_write_template <- function(results = "nops_eval.csv",
 
     ## Images
     dat$image1 <- sprintf("<img src=\"%s\" />",
-                      base64enc::dataURI(file = file.path(odir, results[id, "scan"]),
+                      base64enc::dataURI(file = file.path(work_dir, results[id, "scan"]),
                                          mime = "image/png"))
+    if (return_scan) {
+      file.copy(file.path(work_dir, results[id, "scan"]), 
+                file.path(temp_dir, ac, "scan1.png"))
+    }
+    
     if (nscans > 1L && results[id, "scan2"] != "") {
       dat$image2 <-
         sprintf("<img src=\"%s\" />",
-                base64enc::dataURI(file = file.path(odir, results[id, "scan2"]),
+                base64enc::dataURI(file = file.path(work_dir, results[id, "scan2"]),
                                    mime = "image/png"))
+      if (return_scan) {
+        file.copy(file.path(work_dir, results[id, "scan2"]), 
+                  file.path(temp_dir, ac, "scan2.png"))
+      }
     }
 
     template_i <- whisker::whisker.render(template, dat)
-    writeLines(template_i, file.path(temp_dir, ac, out_file))
+    writeLines(template_i, file.path(temp_dir, ac, report_name))
   }
 
-  invisible(zip(file.path(dir, out_zip), c(results[, "id"])))
-
+  if (isTRUE(zip)) {
+    out_zip <- paste0(out_name, ".zip")
+    setwd(temp_dir); on.exit(setwd(work_dir))
+    invisible(zip(file.path(dir, out_zip), results[, "id"]))
+  } else {
+    out_dir <- file.path(dir,  out_name)
+    dir.create(out_dir)
+    invisible(file.copy(file.path(temp_dir, results[, "id"]), 
+                        out_dir, recursive = TRUE))
+  }
+  
+  unlink(temp_dir, recursive = TRUE)
 }
