57e18f4ca9e41705fcd97a1f28a19dfb6d876231
florianstampfer (Florian Stampfer) <florian.stampfer@uibk.ac.at>
2020-12-29 21:58:51 +0000

    start read.dcf clone
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/exams@2086 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/examsapp/playground/export-standalone.R
+++ pkg/examsapp/playground/export-standalone.R
@@ -1,5 +1,5 @@
 library(shiny)
-library(purrr)
+#library(purrr)
 library(exams)
 
 ########
@@ -30,7 +30,98 @@ library(exams)
 # x
 # saveRDS(x,file = "exams2html.rds")
 
-###########
+
+
+# x <- readRDS("exams2html.rds")
+# x <- cbind(argument = names(x), do.call("rbind", x))
+# colnames(x)[2] <- "description"
+# write.dcf(x, file = "exams2html.dcf", width = Inf) 
+# 
+# x <- readRDS("exams2pdf.rds")
+# x <- cbind(argument = names(x), do.call("rbind", x))
+# colnames(x)[2] <- "description"
+# write.dcf(x, file = "exams2pdf.dcf", width = Inf) 
+# 
+# y <- read.dcf("exams2html.dcf")
+# z <- read.dcf("exams2pdf.dcf")
+
+
+
+
+#######################
+# .assemble_things_into_a_data_frame <- function(tags, vals, 
+#                                                nums) {
+#   tf <- factor(tags, levels = unique(tags))
+#   cnts <- table(nums, tf)
+#   out <- array(NA_character_, dim = dim(cnts), dimnames = list(NULL, 
+#                                                                levels(tf)))
+#   if (all(cnts <= 1L)) {
+#     out[cbind(nums, tf)] <- vals
+#     out <- as.data.frame(out, stringsAsFactors = FALSE)
+#   }
+#   else {
+#     levs <- colSums(cnts > 1L) == 0L
+#     if (any(levs)) {
+#       inds <- tf %in% levels(tf)[levs]
+#       out[cbind(nums[inds], tf[inds])] <- vals[inds]
+#     }
+#     out <- as.data.frame(out, stringsAsFactors = FALSE)
+#     for (l in levels(tf)[!levs]) {
+#       out[[l]] <- rep.int(list(NA_character_), nrow(cnts))
+#       i <- tf == l
+#       out[[l]][unique(nums[i])] <- split(vals[i], nums[i])
+#     }
+#   }
+#   out
+# }
+# 
+# 
+# file <- "exams2html.dcf"
+# 
+# lines <- readLines(file, skipNul = TRUE)
+# ind <- grep("^[^[:blank:]][^:]*$", lines)
+# if (length(ind)) {
+#   lines <- strtrim(lines[ind], 0.7 * getOption("width"))
+#   stop(gettextf("Invalid DCF format.\nRegular lines must have a tag.\nOffending lines start with:\n%s", 
+#                 paste0("  ", lines, collapse = "\n")), domain = NA)
+# }
+# line_is_not_empty <- !grepl("^[[:space:]]*$", lines)
+# nums <- cumsum(diff(c(FALSE, line_is_not_empty) > 0L) > 0L)
+# nums <- nums[line_is_not_empty]
+# lines <- lines[line_is_not_empty]
+# line_is_escaped_blank <- grepl("^[[:space:]]+\\.[[:space:]]*$", 
+#                                lines)
+# if (any(line_is_escaped_blank)) 
+#   lines[line_is_escaped_blank] <- ""
+# line_has_tag <- grepl("^[^[:blank:]][^:]*:", lines)
+# pos <- which(diff(nums) > 0L) + 1L
+# ind <- !line_has_tag[pos]
+# if (any(ind)) {
+#   lines <- strtrim(lines[pos[ind]], 0.7 * getOption("width"))
+#   stop(gettextf("Invalid DCF format.\nContinuation lines must not start a record.\nOffending lines start with:\n%s", 
+#                 paste0("  ", lines, collapse = "\n")), domain = NA)
+# }
+# lengths <- rle(cumsum(line_has_tag))$lengths
+# pos <- cumsum(lengths)
+# tags <- sub(":.*", "", lines[line_has_tag])
+# lines[line_has_tag] <- sub("[^:]*:[[:space:]]*", "", lines[line_has_tag])
+# # fold <- is.na(match(tags, keep.white))
+# # foldable <- rep.int(fold, lengths)
+# # lines[foldable] <- sub("^[[:space:]]*", "", lines[foldable])
+# # lines[foldable] <- sub("[[:space:]]*$", "", lines[foldable])
+# vals <- mapply(function(from, to) paste(lines[from:to], collapse = "\n"), 
+#                c(1L, pos[-length(pos)] + 1L), pos)
+# #vals[fold] <- trimws(vals[fold])
+# out <- .assemble_things_into_a_data_frame(tags, vals, nums[pos])
+# if (!is.null(fields)) 
+#   out <- out[fields]
+# out
+
+
+
+
+
+##############
 
 exportFormatInput <- function(id) {
   exportFormats <- list.files(".",glob2rx("*.rds"))
@@ -92,11 +183,11 @@ examsArgumentServer <- function(id, df) {
     vars <- reactive(names(df()))
     
     output$controls <- renderUI({
-      map(vars(), function(var) make_ui(df()[[var]], NS(id, var), var))
+      lapply(vars(), function(var) make_ui(df()[[var]], NS(id, var), var))
     })
     
     reactive({
-      each_var <- map(vars(), function(var) getArgumentValues(df()[[var]], input[[var]]))
+      each_var <- lapply(vars(), function(var) getArgumentValues(df()[[var]], input[[var]]))
       #each_var <- map(vars(), function(var) input[[var]])
       names(each_var) <- vars()
       #print(each_var)
@@ -106,8 +197,6 @@ examsArgumentServer <- function(id, df) {
   })
 }
 
-myfun = function(x) paste0("Hallo ",x) 
-map
 
 
 examsExportApp <- function() {
