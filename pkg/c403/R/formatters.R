43a08656aa90f121e7162d5ce42c1a67998d1d83
azeileis (Achim Zeileis) <Achim.Zeileis@R-project.org>
2021-04-23 23:59:03 +0000

    move uibkmark to formatters
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/exams@2146 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/c403/R/formatters.R
+++ pkg/c403/R/formatters.R
@@ -1,7 +1,36 @@
-
-#' @param true character, text for true results
-#' @param false character, text for false results
+#' Auxiliary Formatting Functions
 #'
+#' Auxiliary functions for formatting elements of exams.
+#'
+#' @param x numeric (\code{uibkmark}) or logical (\code{mchoice2text}) vector.
+#' @param factor logical. Should the result be a factor or a character?
+#' @param true character. Text for true results.
+#' @param false character. Text for false results.
+#'
+#' @details The function \code{uibkmark} maps the numbers 1 to 5 to the mark labels
+#' SGT1, GUT2, etc. as used by UIBK.
+#'
+#' The function \code{mchoice2text} masks the exams function of the same name
+#' in order to show German text.
+#'
+#' @examples
+#' uibkmark(1:5)
+#' mchoice2text(c(TRUE, FALSE))
+#'
+#' @aliases uibkmark mchoice2text
+#' @keywords utilities
+#'
+#' @export
+uibkmark <- function(x, factor = TRUE) {
+  rval <- factor(x, levels = 1L:5L, labels = c("SGT1", "GUT2", "BEF3", "GEN4", "NGD5"))
+  if(!factor) {
+    rval <- as.character(rval)
+    rval[is.na(rval)] <- ""
+  }
+  return(rval)
+}
+
+
 #' @rdname uibkmark
 #' @export
 mchoice2text <- function(x, true = "\\\\textbf{Richtig}", false = "\\\\textbf{Falsch}")
--- pkg/c403/R/nops_eval.R
+++ pkg/c403/R/nops_eval.R
@@ -1,35 +1,3 @@
-
-#' Auxiliary Formatting Functions
-#'
-#' Auxiliary functions for formatting elements of exams.
-#' \code{mchoice2text(x, true = "\\textbf{Richtig}", false = "\\textbf{Falsch}")}
-#'
-#' @param x numeric (\code{uibkmark}) or logical (\code{mchoice2text}) vector.
-#' @param factor logical. Should the result be a factor or a character?
-#'
-#' @details The function \code{uibkmark} maps the numbers 1 to 5 to the mark labels
-#' SGT1, GUT2, etc. as used by UIBK.
-#'
-#' The function \code{mchoice2text} masks the exams function of the same name
-#' in order to show German text.
-#'
-#' @examples
-#' uibkmark(1:5)
-#' mchoice2text(c(TRUE, FALSE))
-#'
-#' @aliases mchoice2text
-#' @keywords utilities
-#' @export
-uibkmark <- function(x, factor = TRUE) {
-  rval <- factor(x, levels = 1L:5L, labels = c("SGT1", "GUT2", "BEF3", "GEN4", "NGD5"))
-  if(!factor) {
-    rval <- as.character(rval)
-    rval[is.na(rval)] <- ""
-  }
-  return(rval)
-}
-
-
 #' Evaluate NOPS Exams
 #' 
 #' Evaluate NOPS exams produced with \code{\link[c403]{exams2nops}},
