5cb29c5789ae053cfa7f81bf5eb94a42e5eb5528
azeileis (Achim Zeileis) <Achim.Zeileis@R-project.org>
2022-10-07 08:24:39 +0000

    also set warning=FALSE for knitr processing Rmd exercises
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/exams@2424 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/exams/R/xexams.R
+++ pkg/exams/R/xexams.R
@@ -394,7 +394,8 @@ xweave <- function(file, quiet = TRUE, encoding = "UTF-8", engine = NULL,
     })
     oopts <- knitr::opts_chunk$get()
     knitr::opts_chunk$set(dev = dev,
-      fig.height = height, fig.width = width, dpi = resolution, fig.path = "", error = FALSE, highlight = highlight, ...)
+      fig.height = height, fig.width = width, dpi = resolution, fig.path = "",
+      error = FALSE, warning = FALSE, highlight = highlight, ...)
     if(is.null(encoding)) encoding <- getOption("encoding")
     knitr::knit(file, quiet = quiet, envir = envir, encoding = encoding)
     knitr::opts_chunk$set(oopts)
