a76a52e8fb40580d1a246ef26638e154776d51d4
azeileis (Achim Zeileis) <Achim.Zeileis@R-project.org>
2025-07-29 00:12:25 +0000

    also remove converter argument in exams2wooclap() because it is also the default in exams2moodle()
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/exams@2921 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/exams/R/exams2wooclap.R
+++ pkg/exams/R/exams2wooclap.R
@@ -1,11 +1,7 @@
 ## https://docs.wooclap.com/en/articles/1980934-can-i-move-questions-from-wooclap-to-moodle
-exams2wooclap <- function(file, n = 1L, dir = ".", name = "moodletowooclap",
-  converter = "pandoc-mathjax", ...)
+exams2wooclap <- function(file, n = 1L, dir = ".", name = "moodletowooclap",  ...)
 {
-
   ## call exams2moodle
-  rval <- exams2moodle(file = file, n = n, dir = dir, name = name,
-      converter = converter, table= table, flavor = "wooclap", ...)
-
+  rval <- exams2moodle(file = file, n = n, dir = dir, name = name, flavor = "wooclap", ...)
   invisible(rval)
 }
